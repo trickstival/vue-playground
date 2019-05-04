32c076a19bbb5651c3609720317c8bd4e35afaf4
trickstival <trick_stival@hotmail.com>
2019-05-03 21:27:27 -0300

    removed dist from ignore and added demo to readme

---
+++ dist/precache-manifest.020363106b047102e4bf7aa619f6fe18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "532f66a6c537244285ce",
+    "url": "/js/about.80c5f3a1.js"
+  },
+  {
+    "revision": "7d8005dee51767b6c124",
+    "url": "/css/app.087c6bf4.css"
+  },
+  {
+    "revision": "7d8005dee51767b6c124",
+    "url": "/js/app.63e12bfc.js"
+  },
+  {
+    "revision": "73e92be847fc44e24164",
+    "url": "/css/chunk-vendors.2f2933dc.css"
+  },
+  {
+    "revision": "73e92be847fc44e24164",
+    "url": "/js/chunk-vendors.c82adda9.js"
+  },
+  {
+    "revision": "207b682b69dbc2d77d03c540c92d7f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
